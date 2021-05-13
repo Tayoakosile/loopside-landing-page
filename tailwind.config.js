495a7825f728c13b163974944c5dfc0a835adbb3
Tayo Akosile <akosilety9@gmail.com>
2021-05-12 18:27:01 -0600

    Create:Created new Frontend mentor project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      black: 'hsl(0, 0%, 0%)',
+      darkGray: 'hsl(0, 0%, 55%)',
+      darkerGray: 'hsl(0, 0%, 41%)',
+    },
+    fontFamily: {
+      primary: ['Josefin Sans', 'sans-serif'],
+      secondary: ['Alata', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
